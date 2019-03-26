6667bf08f70b5d221242f17a0e882a4508377049
crisnicogonzalez <cristian3629@gmail.com>
2019-03-25 23:56:45 -0300

    OptimumWeatherConditionTest added

---
+++ domain/src/test/java/com/predictor/condition/OptimumWeatherConditionTest.java
@@ -0,0 +1,74 @@
+package com.predictor.condition;
+
+import com.predictor.universe.Planet;
+import com.predictor.universe.Position;
+import com.predictor.universe.SolarSystem;
+import com.predictor.universe.Sun;
+import com.predictor.util.GeometryUtil;
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.predictor.universe.Planet.BETASOID;
+import static com.predictor.universe.Planet.FERENGI;
+import static com.predictor.universe.Planet.VULCANO;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.testng.Assert.*;
+
+@Test
+public class OptimumWeatherConditionTest {
+
+
+    @InjectMocks
+    private OptimumWeatherCondition target;
+    @Mock
+    private GeometryUtil geometryUtil;
+    private Sun sun;
+    private List<Planet> planets;
+    private SolarSystem solarSystem;
+
+    @BeforeClass
+    private void init(){
+        initMocks(this);
+        solarSystem = new SolarSystem(newArrayList(VULCANO, BETASOID, FERENGI),new Sun());
+    }
+
+    @Test
+    public void testMeetsConditionsShouldReturnTrueWhenPlanetsAreAlignedButTheSunIsNot() {
+
+        final Planet planetOne = VULCANO;
+        final Planet planetTwo = FERENGI;
+        final Planet planetThree = BETASOID;
+        final Sun sun = new Sun();
+
+        final Position p1 = new Position(1,0);
+        final Position p2 = new Position(0,2);
+        final Position p3 = new Position(0,0);
+        final Position p4 = new Position(0,0);
+
+
+        planetOne.setPosition(p1);
+        planetTwo.setPosition(p2);
+        planetThree.setPosition(p3);
+        sun.setPosition(p4);
+
+        when(geometryUtil.formALine(eq(newArrayList(p1,p2,p3)))).thenReturn(true);
+        when(geometryUtil.formALine(eq(newArrayList(p1,p2,p3,p4)))).thenReturn(false);
+
+        assertFalse(target.meetsConditions(solarSystem,0));
+
+    }
+
+    @Test
+    public void testGetPrediction() throws Exception {
+    }
+}
